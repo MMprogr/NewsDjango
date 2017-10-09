ba1319b828b49be55c942d5ce7220af5f4276e52
mmprogr <mmprogr@gmail.com>
2017-10-10 00:19:56 +0200

    registration

--- newsclientdjango/models.py
+++ newsclientdjango/models.py
@@ -2,13 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-class NewsManager(models.Manager):
-    def get_by_natural_key(self, title):
-        return self.get(title=title)
-
-
 class News(models.Model):
-    objects = NewsManager()
     title = models.CharField(max_length=100)
     content = models.TextField()
     author = models.ForeignKey(User, related_name='news')
@@ -16,6 +10,3 @@ class News(models.Model):
 
     def __str__(self):
         return 'News with title: {}'.format(self.title)
-
-    def natural_key(self):
-        return self.title
